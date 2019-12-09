fa217afef8ae5345cca09000bea00bdfb86c58d0
Zachary Rangel <zacharyr@ieee.org>
2019-12-09 05:04:05 -0600

    Fixed format strings, removed comments.

--- rootlocusexplorer.py
+++ rootlocusexplorer.py
@@ -1,11 +1,10 @@
 import tkinter as tkinter
-from tkinter import ttk
-from tkinter import *
-
 import matplotlib as matplotlib
 from matplotlib import pyplot as plt
 import numpy as np
 import control as control
+from tkinter import ttk
+from tkinter import *
 
 plt.style.use('classic')
 def plot():
@@ -14,15 +13,7 @@ def plot():
 
     numerator_tf_arg = [float(zeroes) for zeroes in numerator_tf_arg]
     denominator_tf_arg = [float(poles) for poles in denominator_tf_arg]
-    #transfer_func = control.TransferFunction((numerator_tf_arg, (1, 5))
-    #rlist, klist = control.rlocus(control.TransferFunction(((numerator_tf_arg), (1, 5)), kvect=np.linspace(100.0, -100.0, num=1000))
-    #rlist, klist = control.rlocus(control.TransferFunction((1, 45.7), (1, 5, 20, 0)), kvect=np.linspace(100.0, -100.0, num=1000))
-    #control.root_locus(control.TransferFunction(numerator_tf_arg, denominator_tf_arg), kvect=np.linspace(100.0, -100.0, num=1000), Plot=True)
-    #matplotlib.fig(1)
     rlist, klist = control.root_locus(control.TransferFunction(numerator_tf_arg, denominator_tf_arg), kvect=np.linspace(100.0, -100.0, num=1000))
-  #  canvas = FigureCanvasTkAgg(plt, master_window)
-   # canvas.get_tk_widget().grid(row=1,column=24)
-    #canvas.draw()
     try:
         plt.show()
     except:
@@ -33,6 +24,7 @@ master_window = tkinter.Tk()
 master_window.title("Root Locus Explorer") # window name
 
 center_frame = tkinter.ttk.Frame(master_window, padding='20 20 20 20') # frame widget
+#center_frame = tkinter.ttk.Frame(master_window, width=500, height=500, padding='20 20 20 20') # frame widget
 center_frame.grid(column=0, row=0, sticky=('N, W, E, S'))
 
 master_window.columnconfigure(0, weight=1)
@@ -43,14 +35,20 @@ numerator = StringVar()
 denominator = StringVar()
 
 sys_numerator_entry = tkinter.ttk.Entry(center_frame, width=10, textvariable=numerator) # width controls the entry box size
-sys_denominator_entry = tkinter.ttk.Entry(center_frame, width=5, textvariable=denominator)
+sys_denominator_entry = tkinter.ttk.Entry(center_frame, width=10, textvariable=denominator)
+
 
 # define placement of grids for bound vars
-sys_numerator_entry.grid(column=2, row=1, sticky=(W, E))
+sys_numerator_entry.grid(column=2, row=4, sticky=(W, E))
 sys_denominator_entry.grid(column=2, row=8, sticky=(W, E))
 
 # configure entry button. this button executes whatever function "command" is
-test_button = tkinter.ttk.Button(center_frame, text='Plot test', command=plot)
+test_button = tkinter.ttk.Button(center_frame, text='Plot Root Locus', command=plot)
 test_button.grid(column=14, row=1, sticky=W)
+
+ttk.Label(center_frame, text="Input transfer function, separated by spaces\n\n").grid(column=1, row=1, sticky=E)
+ttk.Label(center_frame, text="numerator").grid(column=3, row=4, sticky=W)
+ttk.Label(center_frame, text="denominator").grid(column=3, row=8, sticky=W)
+
 
 master_window.mainloop()
